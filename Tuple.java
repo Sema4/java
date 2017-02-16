012d1c092fe7e2021d906db0f043c2bb41df9996
Andrew Dean <dean411@hotmail.com>
2017-02-15 22:07:04 -0500

    Added getters and setters

--- Tuple.java
+++ Tuple.java
@@ -1,7 +1,20 @@
 public class Tuple{
-  private int xNum;
-  private int yNum;
+	private int xNum;
+	private int yNum;
 
+	public void setxNum(int xNum){
+		this.xNum = xNum;
+	}
 
+	public void setyNum(int yNum){
+		this.yNum = yNum;
+	}
 
+	public int getxNum(){
+		return this.xNum;
+	}
+
+	public int getyNum(){
+		return this.yNum;
+	}
 }
